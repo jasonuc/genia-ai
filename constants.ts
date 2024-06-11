7ca542c136ce3b2cc05fd2be864ae9039a68a4e5
jasonuc <mrjasonu@gmail.com>
2024-06-11 09:06:03 +0100

    feat: increases the free trial count

--- constants.ts
+++ constants.ts
@@ -1,6 +1,6 @@
 import { Code, ImageIcon, MessageSquare, MusicIcon, VideoIcon } from "lucide-react";
 
-export const MAX_FREE_COUNTS = 4;
+export const MAX_FREE_COUNTS = 5;
 
 export interface ToolInterface {
     label: string;
--- lib/utils.ts
+++ lib/utils.ts
@@ -6,5 +6,8 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function absoluteUrl(path: string) {
+  if (process.env.NODE_ENV === "development") {
+    return `${process.env.NEXT_PUBLIC_APP_DEV_URL}${path}`
+    } 
   return `${process.env.NEXT_PUBLIC_APP_URL}${path}`
 }
